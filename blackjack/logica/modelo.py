0985958bb3fe3cd7678f71b753fe75f3619873f7
SamGa <samgarcia04@hotmail.com>
2023-09-08 18:09:47 -0500

    Initial Commit

--- blackjack/logica/modelo.py
+++ blackjack/logica/modelo.py
@@ -13,4 +13,32 @@ class Mano:
         pass
 
 class Baraja:
-    def
+    def __init__(self):
+        self.cartas: list[Carta] = []
+
+    def revolver(self):
+        pass
+    def repartir_carta(self):
+        pass
+
+class Jugador:
+    def __init__(self, nombre: str, fichas: int):
+        self.nombre: str = nombre
+        self.fichas: int = fichas
+        self.mano: Mano = None
+
+    def inicializar_mano(self, cartas: tuple[Carta, Carta]):
+        pass
+
+class Casa:
+    def __init__(self):
+        self.mano: Mano = None
+
+    def inicializar_mano(self, cartas: tuple[Carta, Carta]):
+        pass
+
+class Blackjack:
+    def __init__(self):
+        self.jugador: Jugador = None
+        self.cupier: Casa = None
+        self.baraja: Baraja = None
