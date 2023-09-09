d400aa02e1436de23cb73dfc5fec39eb42ff419a
SamGa <samgarcia04@hotmail.com>
2023-09-08 21:41:55 -0500

    Desarrolo del R3 "Hacer jugada del jugador"

--- blackjack/logica/modelo.py
+++ blackjack/logica/modelo.py
@@ -42,3 +42,41 @@ class Blackjack:
         self.jugador: Jugador = None
         self.cupier: Casa = None
         self.baraja: Baraja = None
+
+def solicitar_jugada_jugador(self):
+    # 1. El sistema solicita al jugador que seleccione una jugada
+    while True:
+        jugada = input("¿Qué desea hacer? (h) para pedir carta, (p) para plantarse, (d) para doblar o (s) para rendirse: ")
+        if jugada in ["h", "p", "d", "s"]:
+            break
+    # 2. Si el jugador pide una carta
+    if jugada == "h":
+        self.pedir_carta_jugador()
+    # 3. Si el jugador se planta
+    elif jugada == "p":
+        self.plantarse_jugador()
+    # 4. Si el jugador dobla
+    elif jugada == "d":
+        self.doblar_jugador()
+    # 5. Si el jugador se rinde
+    elif jugada == "s":
+        self.rendirse_jugador()
+def pedir_carta_jugador(self):
+    # 3.1 El sistema le reparte una carta a la mano del jugador
+    self.jugador.mano.append(self.mazo.pop())
+    # 3.2 Si el valor de la mano del jugador es mayor a 21
+    if self.jugador.valor_mano() > 21:
+        self.finalizar_juego()
+    # 3.3 Si el valor de la mano no supera 21
+    else:
+        self.solicitar_jugada_jugador()
+def plantarse_jugador(self):
+    # 4.1 Se calcular el valor de la mano, se muestra y se guarda
+    self.jugador.calcular_valor_mano()
+    # 4.2 Se ejecuta el requisito R4 hacer jugada de la casa
+    self.hacer_jugada_casa()
+def doblar_jugador(self):
+    # 4.1 Se calcular el valor de la mano, se muestra y se guarda
+    self.jugador.calcular_valor_mano()
+    # 4.2 Se ejecuta el requisito R4 hacer jugada de la casa
+    self.hacer_jugada_casa()
